e3452f44fdcfff39da38a82b9074965cd1385628
Ariadiny Paulino <ariadiny.miranda@gmail.com>
2016-05-16 11:05:42 -0400

    added another device, changed the paths to be inside the polygon

--- js/mainController.js
+++ js/mainController.js
@@ -11,27 +11,45 @@ app.controller('mainController', function($scope) {
                     'initialY': 0,
                     'pathId': 'svg_device1',
                     'image': 'img/butterfly_32x32.png',
-                    'path': 'M1,216 L240,45 L480,216 L387,495 L96,495 L1,216 z',
+                    'path': 'M10,256 L290,95 L230,266 L127,350 L136,225 L10,256 z',
                     'name': 'Ady iPhone',
                     'battery': '100%'};
+    var device2 = {'width': 32, 
+                    'height': 32,
+                    'initialX': 0,
+                    'initialY': 0,
+                    'pathId': 'svg_device2',
+                    'image': 'img/icon.svg',
+                    'path': 'M240,45 L1,216 L320,116 L387,295 L96,295 L1,216 z',
+                    'name': 'Lalit iPhone',
+                    'battery': '35%'};
+    var device3 = {'width': 32, 
+                    'height': 32,
+                    'initialX': 0,
+                    'initialY': 0,
+                    'pathId': 'svg_device3',
+                    'image': 'img/iphone_32x32.png',
+                    'path': 'M320,116 L1,216 L240,45 L387,295 L96,295 L320,116 z',
+                    'name': 'Lalit iPhone',
+                    'battery': '35%'};
     
-    $scope.deviceList = [device1];
+    $scope.deviceList = [device1, device2, device3];
     
     $scope.showInfo = false;
     $scope.showDeviceInfo = function(id) {
-        if (id == 1){
-            $scope.device.battery = '100%';
-            $scope.device.name = 'Ady iPhone';
-            $scope.device.id = id;
-        } else {
-            $scope.device.battery = '35%';
-            $scope.device.name = 'Other iPhone';
-            $scope.device.id = id;
-        }
+        getDeviceInfo(id);
         $scope.showInfo = true;
     };
     
     $scope.showDeviceName = function(id) {
+        angular.forEach($scope.deviceList, function(value, key) {
+            if(value.pathId == id){
+                $scope.deviceName = value.name;
+            } 
+        });
+    };
+    
+    getDeviceInfo = function(id) {
         angular.forEach($scope.deviceList, function(value, key) {
             if(value.pathId == id){
                 $scope.device = value;
