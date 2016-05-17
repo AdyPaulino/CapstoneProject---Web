694f6a0574c9a73ca7c19880a92d0eb4e9671089
Ady Paulino <apaulino7908@2A206-C01.conestogac.on.ca>
2016-05-17 11:33:39 -0400

    Added more devices, get beacons from mainController file

--- js/mainController.js
+++ js/mainController.js
@@ -1,10 +1,12 @@
 var app = angular.module('app', []);
 
 app.controller('mainController', function($scope) {
+    //group info
     $scope.groupName = "TAALK";
     $scope.groupMembers = "Tiago, Ady, Alex, Lalit and Kevin";
-    $scope.device = {};
     
+    //device list
+    $scope.device = {};
     var device1 = {'width': 32, 
                     'height': 32,
                     'initialX': 0,
@@ -30,10 +32,29 @@ app.controller('mainController', function($scope) {
                     'pathId': 'svg_device3',
                     'image': 'img/iphone_32x32.png',
                     'path': 'M320,116 L1,216 L240,45 L387,295 L96,295 L320,116 z',
-                    'name': 'Lalit iPhone',
+                    'name': 'Kevin iPhone',
+                    'battery': '35%'};
+    var device4 = {'width': 32, 
+                    'height': 32,
+                    'initialX': 0,
+                    'initialY': 0,
+                    'pathId': 'svg_device4',
+                    'image': 'img/iphone_32x32.png',
+                    'path': 'M387,295 L100,216 L240,45 L230,450 L320,116 L96,295 L100,116 z',
+                    'name': 'Tiago\'s phone',
                     'battery': '35%'};
     
-    $scope.deviceList = [device1, device2, device3];
+    var device5 = {'width': 32, 
+                    'height': 32,
+                    'initialX': 0,
+                    'initialY': 0,
+                    'pathId': 'svg_device5',
+                    'image': 'img/iphone_32x32.png',
+                    'path': 'M127,350 L300,295 L100,216 L240,45 L230,450 L320,116 L96,295 L230,450 z',
+                    'name': 'Yan\'s phone',
+                    'battery': '45%'};
+    
+    $scope.deviceList = [device1, device2, device3, device4, device5];
     
     $scope.showInfo = false;
     $scope.showDeviceInfo = function(id) {
@@ -56,6 +77,27 @@ app.controller('mainController', function($scope) {
             } 
         });
     };
+    
+    //beacon list
+    var beacon1 = {
+        'coordinates': {'x':99, 'y':145},
+        'r': 3,
+        'id': 'pointA'
+    };
+    
+     var beacon2 = {
+        'coordinates': {'x':315, 'y':100},
+        'r': 3,
+        'id': 'pointB'
+    };
+    
+    var beacon3 = {
+        'coordinates': {'x':435, 'y':350},
+        'r': 3,
+        'id': 'pointC'
+    };
+    
+     $scope.beaconList = [beacon1, beacon2, beacon3];
 });
 
 app.directive('tooltip', function(){
